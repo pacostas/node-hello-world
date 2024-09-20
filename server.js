d52cb8aacca84b6ddca64b7816a3b83b7278883d
Costas Papastathis <papastathiscr@gmail.com>
2024-09-20 22:43:57 +0300

    init commit

--- server.js
+++ server.js
@@ -2,25 +2,20 @@ const { createServer } = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
-  if (req.url.match(/\/hello\/node/) && req.method === "GET") {
+  if (req.url.match(/\/health/) && req.method === "GET") {
+    res.writeHead(200);
+    res.end();
+  } else if (req.url.match(/\/live/) && req.method === "GET") {
+    res.writeHead(200);
+    res.end();
+  } else {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
         data: "hello",
       })
     );
-  } else if (req.url.match(/\/health/) && req.method === "GET") {
-    res.writeHead(200);
-    res.end();
-  } else if (req.url.match(/\/live/) && req.method === "GET") {
-    res.writeHead(200);
-    res.end();
   }
 });
-
-function handle(signal) {
-  console.log(`*^!@4=> Received event: ${signal}`);
-}
-process.on("SIGHUP", handle);
 
 server.listen(PORT, () => console.log(`Server running on port:  ${PORT}`));
